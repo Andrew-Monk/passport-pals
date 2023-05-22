7b8ccf9822385f5436d0ae6e86b56ebac6f53932
Stesha Carle <stesha.carle@gmail.com>
2023-05-22 09:31:36 -0700

    code cleaning

--- passport_pals/queries/events.py
+++ passport_pals/queries/events.py
@@ -26,18 +26,4 @@ class EventQueries(Queries):
             return None
         event["id"] = str(event["_id"])
         return EventOut(**event)
-from bson.objectid import ObjectId
-from typing import List
-from .client import Queries
-from models import EventIn, EventOut
 
-class EventQueries(Queries):
-    DB_NAME = "passportpals"
-    COLLECTION = "events"
-
-    def create(self, event: EventIn) -> EventOut:
-        props = event.dict()
-        print(props)
-        self.collection.insert_one(props)
-        props["id"] = str(props["_id"])
-        return EventOut(**props)
