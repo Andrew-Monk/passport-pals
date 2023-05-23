696f609bf930c2675c0cf4a458776339ac989397
Andrew <andrewmonk1991@gmail.com>
2023-05-23 17:30:27 -0500

    navbar added

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import MainPage from "./MainPage";
+import Nav from "./Nav";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <Nav />
         <Routes>
           <Route path="/" element={<MainPage />} />
         </Routes>
