bb25e123f172feedeac9cd8a0b14fbdc50ba7eee
Andrew Monk <andrewmonk1991@gmail.com>
2023-05-23 22:32:25 +0000

    Navbar

---
+++ ghi/src/Nav.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { NavLink} from 'react-router-dom'
+
+
+function Nav(){
+    return(
+
+        <nav className="navbar navbar-expand-lg navbar-dark bg-info navbar-fixed-top">
+        <div className="container-fluid">
+            <div className="row">
+                <ul className="navbar-nav mx-2 me-auto mb-5 mb-lg-3">
+                    <li>
+                        <NavLink className="navbar" to="/events">Events</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className="navbar" to="/account">Account</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className="navbar" to="/login">Login</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className="navbar" to="/logout">Logout</NavLink>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        </nav>
+    )
+}
+
+export default Nav;
