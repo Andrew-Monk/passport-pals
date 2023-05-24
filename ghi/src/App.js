893f0c475ef4e6e75ca345b9067802f02877fd6f
Andrew <andrewmonk1991@gmail.com>
2023-05-24 18:05:34 -0500

    resolved create event form

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import MainPage from "./MainPage";
 import Nav from "./Nav";
+import EventsList from "./EventsList";
+import CreateEventForm from './CreateEventForm'
 
 function App() {
   return (
@@ -11,7 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/events/list" element={<EventsList />} />
-          <Route path="events/create" element={<CreateEventForm />} />
+          <Route path="/events/create" element={<CreateEventForm />} />
         </Routes>
       </BrowserRouter>
     </div>
