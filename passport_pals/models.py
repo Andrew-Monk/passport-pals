0f3a93acd284f562f265b025b04230a0f387a042
Rebecca Tom <rebeccatom8@gmail.com>
2023-05-25 21:25:26 +0000

    Started event detail page

--- passport_pals/models.py
+++ passport_pals/models.py
@@ -1,20 +1,11 @@
 from pydantic import BaseModel
-from typing import List
-
-
-class AccountIn(BaseModel):
-    email: str
-    password: str
-    full_name: str
-
-
-class AccountOut(AccountIn):
-    id: str
-
+from typing import List, Optional
 
 class EventIn(BaseModel):
     event_title: str
     location: str
+    expected_guests: Optional[list]
+    # contains all attendees ids ^^
     picture: str
     category: str
     cost: int
@@ -30,3 +21,16 @@ class EventOut(EventIn):
 
 class EventList(BaseModel):
     events: List[EventOut]
+
+class AccountIn(BaseModel):
+    email: str
+    language: str
+    country: str
+    password: str
+    full_name: str
+    attending: Optional[list]
+    hosting: Optional[list]
+    # can put event id's in these lists ^^
+
+class AccountOut(AccountIn):
+    id: str
