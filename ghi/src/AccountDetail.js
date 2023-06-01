05256d5c013845649835bfaeff6c1ca069aadee2
Andrew <andrewmonk1991@gmail.com>
2023-06-01 16:55:53 -0500

    added event relations to account, verified account has hosted events and event detail links work

--- ghi/src/AccountDetail.js
+++ ghi/src/AccountDetail.js
@@ -1,35 +1,78 @@
 import { useState, useEffect } from "react";
 import useToken from "@galvanize-inc/jwtdown-for-react";
+import { Link } from "react-router-dom";
 
 function AccountDetail() {
   const [accountData, setAccountData] = useState("");
   const { fetchWithCookie } = useToken();
+  const [events, setEvents] = useState([]);
+
+  async function fetchEvents() {
+    const eventsUrl = "http://localhost:8000/api/events/";
+    const response = await fetch(eventsUrl);
+    console.log(response);
+    if (response.ok) {
+      const responseData = await response.json();
+      const eventsData = responseData.events;
+      setEvents(eventsData);
+    }
+  }
+
+  useEffect(() => {
+    fetchEvents();
+  }, []);
 
   const handleFetch = async () => {
     const response = await fetchWithCookie(
       `${process.env.REACT_APP_PASSPORT_PALS_API_HOST}/token`
     );
-    console.log(response)
-        setAccountData(response.account);
+    console.log(response);
+    setAccountData(response.account);
   };
-  useEffect(() =>{
+  useEffect(() => {
     handleFetch();
   }, []);
 
-  return (
-    <>
-      <div>
-        <h1>Welcome, {accountData.full_name}!</h1>
-      </div>
-      <div>
-        <h2>Your Hosted Events</h2>
-        <p>{accountData.language}</p>
-      </div>
-      <div>
-        <h2>Your Upcoming Events</h2>
-      </div>
-    </>
-  );
-};
+    return (
+      <>
+        <div>
+          <h1>Welcome, {accountData.full_name}!</h1>
+        </div>
+        <h2>Events</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Location</th>
+              <th>Picture</th>
+            </tr>
+          </thead>
+          <tbody>
+            {accountData.hosting && accountData.hosting.map((eventId) => {
+                const hostedEvent = events.find(
+                  (event) => event.id === eventId
+                );
+                if (hostedEvent) {
+                  return (
+                    <tr key={hostedEvent.id}>
+                      <td>
+                        <Link to={`/events/${hostedEvent.id}`}>
+                          {hostedEvent.event_title}
+                        </Link>
+                      </td>
+                      <td>{hostedEvent.location}</td>
+                      <td>{hostedEvent.picture}</td>
+                    </tr>
+                  );
+                }
+              })}
+          </tbody>
+        </table>
+        <div>
+          <h2>Your Upcoming Events</h2>
+        </div>
+      </>
+    );
+  };
 
-export default AccountDetail;
+  export default AccountDetail;
